f76657fb59ac447474029c5f69f312673ffb36aa
Chris Shyi <chris.shyi@mail.utoronto.ca>
2019-06-16 14:14:23 +0800

    1st enhancement for hw6
    Finished first enhancement for hw6, the addition of three new Tetris
    blocks.

--- week9/hw6/hw6assignment.rb
+++ week9/hw6/hw6assignment.rb
@@ -5,35 +5,67 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-               [[0, 0], [0, -1], [0, 1], [0, 2]]],
-               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
-              # new blocks
-              + [
-                 [ # 5 block long, two orientations (new block #2)
-                  [[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],
-                  [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]
-                 ],
-                 rotations([[0, 0], [1, 0], [2, 0], [0, 1], [1, 1]]), # new block #1
-                 rotations([[0, 0], [1, 0], [0, 1]]) # new block # 3
+    All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+                rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+                [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+                [[0, 0], [0, -1], [0, 1], [0, 2]]],
+                rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+                rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+                rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+                rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
+                # new blocks
+                [ # 5 block long, two orientations (new block #2)
+                [[0, 0], [-1, 0], [-2, 0], [1, 0], [2, 0]],
+                [[0, 0], [0, -1], [0, -2], [0, 1], [0, 2]]
+                ],
+                rotations([[0, 0], [1, 0], [2, 0], [0, 1], [1, 1]]), # new block #1
+                rotations([[0, 0], [1, 0], [0, 1]]) # new block # 3
                 ]
-  # your enhancements here
+    # your enhancements here
+
+    def self.next_piece(board)
+        MyPiece.new(All_My_Pieces.sample, board)        
+    end
 
 end
 
 class MyBoard < Board
   # your enhancements here
+    def initialize(game)
+        @grid = Array.new(num_rows) {Array.new(num_columns)}
+        @current_block = MyPiece.next_piece(self)
+        @score = 0
+        @game = game
+        @delay = 500
+    end
 
+    def next_piece
+        @current_block = MyPiece.next_piece(self)
+        @current_pos = nil
+    end
+
+    def store_current
+        locations = @current_block.current_rotation
+        displacement = @current_block.position
+        (0..(locations.size - 1)).each{|index| 
+        current = locations[index];
+        @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+        @current_pos[index]
+        }
+        remove_filled
+        @delay = [@delay - 2, 80].max
+    end
 end
 
 class MyTetris < Tetris
   # your enhancements here
-
+    def set_board
+        @canvas = TetrisCanvas.new
+        @board = MyBoard.new(self)
+        @canvas.place(@board.block_size * @board.num_rows + 3,
+                    @board.block_size * @board.num_columns + 6, 24, 80)
+        @board.draw
+    end
 end
 
 
